e8c6f80bce774c5bdeabbeddee9b99d98bea7a28
Liffey24 <oharahek@tcd.ie>
2024-11-14 11:29:02 +0000

    Add files via upload
    current submitted portfolio

---
+++ Portfolio/assets/script/portfolio_script.js
@@ -0,0 +1,13 @@
+// Highlight links on hover
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = 'yellow';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.color = 'white';
+    });
+});
+
